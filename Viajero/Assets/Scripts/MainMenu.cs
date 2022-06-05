f0c58f06abd034a5a943881c7547dd8facfafea4
Jeahfe Amamangpang <40689534+jiammngpng@users.noreply.github.com>
2022-06-05 12:43:40 +0800

    Updates as of June 5

--- Viajero/Assets/Scripts/MainMenu.cs
+++ Viajero/Assets/Scripts/MainMenu.cs
@@ -2,12 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    [SerializeField] private GameObject loadingPanel;
+    [SerializeField] private Slider progressBar;
+    
+    public void Awake()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        loadingPanel.SetActive(false);
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        loadingPanel.SetActive(true);
+        StartCoroutine(LoadLevelAsync(levelName));
+    }
+
+    private IEnumerator LoadLevelAsync(string levelName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelName);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress);
+            progressBar.value = progress;
+
+            Debug.Log(progress);
+            yield return null;
+        }
     }
     
     public void QuitGame()
