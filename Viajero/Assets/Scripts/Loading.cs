e2940a461ec9d5c72fdd35e44ec1ab8ec3021003
jiammngpng <jeahcarin2000@gmail.com>
2022-06-17 19:42:08 +0800

    Added Quizzer and Deployed in Android

--- Viajero/Assets/Scripts/Loading.cs
+++ Viajero/Assets/Scripts/Loading.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Loading : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject LoadingScreen;
+    public Image LoadingBarFill;
+
+    public void LoadScene(int sceneId)
     {
-        
+        StartCoroutine(LoadSceneAsync(sceneId));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadSceneAsync(int sceneId)
     {
-        
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        LoadingScreen.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress/0.9f);
+            LoadingBarFill.fillAmount = progressValue;
+            yield return null;
+        }
     }
 }
